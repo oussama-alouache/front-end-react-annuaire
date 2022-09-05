98b074f4fa3546ceb93f70f6b09a844e01d45ae7
oussama-alouache <oussamaalouacheedv@gmail.com>
2022-09-05 18:53:56 +0200

    fixing css

--- src/network/pages/login-register/register.js
+++ src/network/pages/login-register/register.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
-
+import'./assets/register-style.css'
 
 export class Register extends Component {
   render() {
     return (
    
     <form className="register-form">
-        <h3>Login Here</h3>
+      <div className="head-register" >
+      <h3>Login Here</h3>
+      </div>
+       
 
         <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username"></input>
+        <input className="input-register" type="text" placeholder="Email or Phone" id="username"></input>
 
         <label htmlFor="email">email</label>
-        <input type="email" placeholder="Email" id="email"></input>
+        <input className="input-register" type="email" placeholder="Email" id="email"></input>
         <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password"></input>
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password"></input>
-        <label htmlFor="password">Password</label>
-        <input type="password" placeholder="Password" id="password"></input>
+        <input className="input-register" type="password" placeholder="Password" id="password"></input>
+        <label htmlFor="password"> conforme Password</label>
+        <input className="input-register" type="password" placeholder=" conforme Password" id="password"></input>
+        <label htmlFor="email">role</label>  
+        <select className="input-register" name=" optionlist " >
+        <option>chose a role....</option>
+        <option>ceo</option>
+        <option>admin</option>
+        <option> option 4 </option>
+        <option> option 5 </option>
+        </select>
 
-        <button className="btn-login">Log In</button>
+        <button type="submit" className="btn-register">Log In</button>
        
     </form>
 
